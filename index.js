d914c0aad3872442931100ae0ac656a2c36ddae0
wiisv <asim.elsanosi@gmail.com>
2024-05-24 19:20:01 +0400

    Third commit.

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.use('/public', express.static(`${process.cwd()}/public`))
 app.use(express.urlencoded({ extended: true }))
 
 const data = {
-  freecodecamp: { original_url: 'https://freecodecamp.org', short_url: 1 },
-  google: { original_url: 'https://google.com', short_url: 2 },
+  freecodecamp: { original_url: 'https://www.freecodecamp.org', short_url: 1 },
+  google: { original_url: 'https://www.google.com', short_url: 2 },
   forums: { original_url: 'https://forum.freecodecamp.org/', short_url: 3 }
 }
 

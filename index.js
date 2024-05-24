a50be86076ecaf5d4ff1bafc342315e7c39e3c4c
wiisv <asim.elsanosi@gmail.com>
2024-05-24 20:56:22 +0400

    .

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000
 app.use(cors())
 
 app.use('/public', express.static(`${process.cwd()}/public`))
-// app.use(express.json())
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 const data = {
